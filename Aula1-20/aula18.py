46359fca29bc5101e70394dfcf77a5dace388d78
Guihassen <guihassenpflug@gmail.com>
2024-01-02 14:55:00 -0300

    commit

---
+++ Aula1-20/aula18.py
@@ -0,0 +1,9 @@
+# Uso dos operadores In e Not In
+
+entrada = input("Digite [E] para Entrar e [S] para Sair: ")
+
+if "E" in entrada :
+    print("Seja Bem vindo!")
+
+if "E" not in entrada :
+    print("Adeus!")
---
+++ Aula1-20/aula19.py
@@ -0,0 +1,11 @@
+#Interpolação de Strings
+# %s = string
+# %i e %d = int
+# %f = float 
+# %x ou %X = hexadecimal
+
+nome = "Guilerme"
+altura = 1.81
+dinheiro = 1000
+
+print("%s tem %.2f de altura e possui %i de reais na conta" %(nome, altura, dinheiro))

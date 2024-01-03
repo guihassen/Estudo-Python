885281b03a1f4e9983b9123484610f901309afde
guihassen <gui_steam@yahoo.com>
2024-01-03 03:27:57 -0300

    commit

--- aula28.py
+++ aula28.py
@@ -1,1 +1,11 @@
 # uso de while e break
+
+senha_permitida = "Guilerme"
+senha_digitada = input("Digite sua senha: ")
+login = senha_digitada == senha_permitida
+
+while login == False :
+    senha_digitada = input("Senha Incorreta! Digite novamente: ")
+    if senha_digitada == senha_permitida :
+        print ("Bem Vindo !")
+        break 
---
+++ aula29.py
@@ -0,0 +1,9 @@
+#Continuacao do uso do while
+
+contador = 0
+
+while contador <= 10 :
+    
+    print(contador)
+    contador =  contador + 1
+print("Fim")

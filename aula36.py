3e4e0c9951e1ace24e953fc95df650bf986cd6af
guihassen <gui_steam@yahoo.com>
2024-01-05 16:34:09 -0300

    commit

---
+++ aula36.py
@@ -0,0 +1,12 @@
+"""
+
+Listas em Python
+tipo list - mutavel
+suporta valores de qualquer tipo
+conhecimentos reutilizaveis - indices e fatimento
+metodos uteis: append, insert, pop, del, clear, extend
+
+"""
+
+string = "ABCDE"
+lista = [123, True, "Guilerme", 1.2]

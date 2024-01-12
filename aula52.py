6f0ee1a39dabd1764c3bbc644964e4d1429aaaae
guihassen <gui_steam@yahoo.com>
2024-01-12 14:53:13 -0300

    commit

---
+++ aula52.py
@@ -0,0 +1,16 @@
+#Introdução ao comando def
+
+
+def imprimir(a,b,c) :
+    print(a,b,c)
+
+
+imprimir(1,2,3)
+imprimir(4,5,6)
+
+
+def saudacao(nome) :
+    print(f"Ola, {nome}")
+
+
+saudacao("Guilerme")

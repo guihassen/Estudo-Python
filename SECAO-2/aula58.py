624d7a8fb2f8b11014c9a549399e4a8cdf5135c1
Guilherme <gui_steam@yahoo.com>
2024-01-14 17:34:56 -0300

    commit

---
+++ SECAO-2/aula58.py
@@ -0,0 +1,24 @@
+"""
+High Order Functions
+Funcoes de primeira classe
+
+Academicamente, os termos Higher Order Functions e First-Class Functions têm significados diferentes.
+
+Higher Order Functions - Funções que podem receber e/ou retornar outras funções
+
+First-Class Functions - Funções que são tratadas como outros tipos de dados comuns (strings, inteiros, etc...)
+"""
+
+def saudacao(msg, nome):
+    return f'{msg}, {nome}!'
+
+
+def executa(funcao, *args):
+    return funcao(*args)
+
+
+print(
+    executa(saudacao, 'Bom dia', 'Luiz')
+)
+print(
+    executa(saudacao, 'Boa noite', 'Maria')

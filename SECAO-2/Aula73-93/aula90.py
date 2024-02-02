f1602a173032eb054e0ba5d30573ba3d8fa22b4f
guihassen <gui_steam@yahoo.com>
2024-02-01 22:13:54 -0300

    commit

---
+++ SECAO-2/Aula73-93/aula90.py
@@ -0,0 +1,9 @@
+import aula90_m
+from aula90_m import soma, variavel_modulo
+
+print('Este módulo se chama', __name__)
+# print('Este módulo se chama', __name__)
+print(aula90_m.variavel_modulo)
+print(variavel_modulo)
+print(soma(2, 3))
+print(aula90_m.soma(2, 3))

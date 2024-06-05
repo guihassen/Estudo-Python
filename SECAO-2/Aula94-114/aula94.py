1cf8bf48e322efa43d8323daabe69e5c9f7af608
guihassen <gui_steam@yahoo.com>
2024-06-05 12:20:11 -0300

    commit

--- SECAO-2/Aula94-114/aula94.py
+++ SECAO-2/Aula94-114/aula94.py
@@ -4,26 +4,25 @@
 # Decoradores são usados para fazer o Python
 # usar as funções decoradoras em outras funções.
 
-def criar_funcao(func):
-    def interna(*args, **kwargs):
-        print('Vou te decorar')
-        for arg in args:
-            e_string(arg)
-        resultado = func(*args, **kwargs)
-        print(f'O seu resultado foi {resultado}.')
-        print('Ok, agora você foi decorada')
+
+def create_function(func):
+    def interna(*args,**kwargs):
+        for arg in args :
+            is_string(arg)
+        resultado = func(*args,**kwargs)
+        print(f"O seu resultado foi {resultado}")
         return resultado
     return interna
 
 
-def inverte_string(string):
+def inverte_string (string) :
     return string[::-1]
 
+def is_string (param):
+    if not isinstance(param,str):
+        raise TypeError("Deve ser uma string")
 
-def e_string(param):
-    if not isinstance(param, str):
-        raise TypeError('param deve ser uma string')
-
-
-inverte_string_checando_parametro = criar_funcao(inverte_string)
-invertida = inverte_string_checando_parametro('123')
+string_check = create_function(inverte_string)
+invertida = inverte_string("4321")
+print(invertida)
+    
